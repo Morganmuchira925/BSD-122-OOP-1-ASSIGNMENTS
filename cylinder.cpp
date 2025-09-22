7cd8784b9c65ac09de28c6675c5c2728471bfde4
Morgan Muchira <morganisrael2005@gmail.com>
2025-09-22 12:29:41 +0300

    Cylinder and Product using classes and objects

---
+++ cylinder.cpp
@@ -0,0 +1,30 @@
+/*Name: Morgan Muchira
+Reg No:BCS-03-0093/2025
+Desc: Surface area and volume with classes and objects
+*/
+#include<iostream>
+using namespace std;
+
+class cylinder{
+    public:
+    float radius, height;
+    // member functions
+    // volume = πr^2h
+    // surface area = 2πr2 + 2πrh
+    double volume(){
+        return 3.14 * radius* radius * height;
+    };
+    double surfaceArea(){
+        return 2 * 3.14 * radius * radius * height + 2 * 3.14 * radius * height;
+    };
+};
+
+int main() {
+    cylinder cyl1;
+    cyl1.radius = 5;
+    cyl1.height = 10;
+
+    cout<<"The volume of the cylinder is "<<cyl1.volume()<<endl;
+    cout<<"The surface area of the cylinder is "<<cyl1.surfaceArea()<<endl;
+    return 0;
+}
---
+++ product.cpp
@@ -0,0 +1,27 @@
+/*Name: Morgan Muchira
+Reg No:BCS-03-0093/2025
+Description: Product of three numbers using classes and objects
+*/
+#include<iostream>
+using namespace std;
+
+class Product{
+    public:
+    float num1, num2, num3;
+    // member function
+    double calculateProduct(){
+        return num1 * num2 * num3;
+    };
+};
+
+int main() {
+    Product prod1;
+    int TotalProduct;
+    prod1.num1 = 5;
+    prod1.num2 = 10;
+    prod1.num3 = 15;
+
+    TotalProduct = prod1.calculateProduct();
+    cout<<"The product of the three numbers is "<<TotalProduct <<endl;
+    return 0;
+}
